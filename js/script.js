b798e5120cc3fb8ed214e09075836debf876e5ef
puhrome <phirom.yim@fullsail.edu>
2014-05-15 15:41:19 -0400

    Basic Expressions
    Examples of basic expressions using variables and assignment operators
    in JS through WebStorm

---
+++ js/script.js
@@ -0,0 +1,15 @@
+/**
+ * Basic Expressions Created by Phirom Yim on 5/15/14.
+ */
+//expressions are sequential
+//moving and manipulating data
+
+var a = 2;//sets up variable and defines with 2
+/*a + 3;//add 3 to a, this is wrong
+*/
+
+a = a + 3;// assigning the value of a plus 3 by including an assignment operator
+console.log(a);//prints the value of a but not adding the 3
+//need an assignment operator to work
+
+//write an expression that adds 3 to operator
